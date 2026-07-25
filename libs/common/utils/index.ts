994ef77a834a7ed518c0a98a0429cfc980c9c89e
trannhon70 <kevintran351996@gmail.com>
2026-07-25 07:54:50 +0700

    update

--- libs/common/utils/index.ts
+++ libs/common/utils/index.ts
@@ -1,6 +1,6 @@
 
 export const expiresIn = '365d' //Refresh Token (365 ngày)
 
-export const REFRESH_TTL = 30 * 24 * 60 * 60; // 30 ngày, khớp expiresIn refresh token
+export const REFRESH_TTL = 365 * 24 * 60 * 60; // 365 ngày, khớp expiresIn refresh token
 
 export const accessExpire = 60 * 60; // 1 giờ (giây)
